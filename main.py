32b90233b390f9d9853f73cc225bae0bf2a1040f
AlessandroKuz <akuzcontact@gmail.com>
2025-04-11 09:11:20 +0200

    feat: add function to sort dict by items count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_words_number, get_char_number
+from stats import get_words_number, get_char_number, get_sorted_items_dict
 
 
 def get_book_text(filepath:str) -> str:
@@ -10,7 +10,11 @@ def main():
     contents: str = get_book_text('./books/frankenstein.txt')
     num_words: int = get_words_number(contents)
     char_dict: dict[str, int] = get_char_number(contents)
-    print(char_dict)
+    sorted_char_dict: dict[str, int] = get_sorted_items_dict(char_dict)
+    
+    for character, value in sorted_char_dict.items():
+        if character.isalpha():
+            print(f'{character}: {value}')
 
 if __name__ == '__main__':
     main()
--- stats.py
+++ stats.py
@@ -13,3 +13,12 @@ def get_char_number(text:str) -> dict[str, int]:
     
     return char_counts
 
+def get_sorted_items_dict(items_dict:dict[str, int]) -> dict[str, int]:
+    sorted_chars: list[dict[str, str | int]] = [
+        {'name': char, 'num': num} for char, num in items_dict.items()
+    ]
+    sorted_chars.sort(key=lambda x: x["num"], reverse=True)
+    sorted_char_dict: dict[str, int] = {dictionary['name']: dictionary['num'] for dictionary in sorted_chars}
+
+    return sorted_char_dict
+
