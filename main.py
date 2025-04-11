68a5f212aecaa7dbdfcec363e0db026008b8dbca
AlessandroKuz <akuzcontact@gmail.com>
2025-04-11 09:03:47 +0200

    feat: add text extraction function and reformat main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath:str) -> str:
+    with open(filepath, 'r') as f:
+        file_contents: str = f.read()
+    return file_contents
+
+def main():
+    contents: str = get_book_text('./books/frankenstein.txt')
+    print(contents)
+
+if __name__ == '__main__':
+    main()
+
