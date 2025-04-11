924788ea02606b330ebef595bc5f5095e29ccd2d
AlessandroKuz <akuzcontact@gmail.com>
2025-04-11 09:15:28 +0200

    feat: add print report function

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ def get_book_text(filepath:str) -> str:
         file_contents: str = f.read()
     return file_contents
 
+def print_report(num_words:int, sorted_char_dict:dict[str, int]):
+    print('============ BOOKBOT ============')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
+    print(f"Found {num_words} total words")
+    print('--------- Character Count -------')
+    for character, value in sorted_char_dict.items():
+        if character.isalpha():
+            print(f'{character}: {value}')
+
 def main():
     contents: str = get_book_text('./books/frankenstein.txt')
     num_words: int = get_words_number(contents)
     char_dict: dict[str, int] = get_char_number(contents)
     sorted_char_dict: dict[str, int] = get_sorted_items_dict(char_dict)
     
-    for character, value in sorted_char_dict.items():
-        if character.isalpha():
-            print(f'{character}: {value}')
+    print_report(num_words, sorted_char_dict)
 
 if __name__ == '__main__':
     main()
